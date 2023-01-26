511f38e52988fd3ec6fb5f109f5c74b2e4829041
darichkeul <123261385+darichkeul@users.noreply.github.com>
2023-01-26 11:08:31 +0100

    Update AuthMiddleware.js

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -2,17 +2,17 @@ const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
     try {
-        // const token = req.headers.authorization.split(' ')[1];
-        // // console.log(token);
-        // if (!token) {
-        //     throw (new Error('token not valid'))
-        // }
-        // const decodedToken = jwt.verify(token, `${process.env.SECRET}`);
-        // if (!decodedToken) {
-        //     throw (new Error('Unauthorized'))
-        // }
-        // // console.log(decodedToken.id);
-        // res.locals.userId = decodedToken.id;
+        const token = req.headers.authorization.split(' ')[1];
+        // console.log(token);
+        if (!token) {
+            throw (new Error('token not valid'))
+        }
+        const decodedToken = jwt.verify(token, `${process.env.SECRET}`);
+        if (!decodedToken) {
+            throw (new Error('Unauthorized'))
+        }
+        // console.log(decodedToken.id);
+        res.locals.userId = decodedToken.id;
         next();
     } catch (error) {
         res.status(401).json({ error: error.message })
